5416ac8e92c1d4f896dbf85de1c3a883e7a81c9b
ethan <zhanyifeng@yeah.com>
2016-01-01 21:26:07 +0800

    update

--- native_bayes/math_oper.py
+++
@@ -1,9 +0,0 @@
-import math
-
-def mean(numbers):
-    return sum(numbers)/float(len(numbers))
-
-def stddev(numbers):
-    avg = mean(numbers)
-    variance = sum([pow(x-avg,2) for x in numbers])/float(len(numbers)-1)
-    return math.sqrt(variance)
